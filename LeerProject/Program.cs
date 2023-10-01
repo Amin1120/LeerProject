f2efddfd1f516e9dd0677e92fc2562a2dc179155
arshia <arshia_esma@hotmail.com>
2023-10-01 22:14:42 +0200

    commit all the rest

---
+++ LeerProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeerProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
